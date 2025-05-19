419e7c642f4f77869f7d0a19ad60c6e1a6fe7ab8
cikristian <rwibutsohakizinkachristian@gmail.com>
2025-05-19 14:52:12 +0200

    aggregation

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,14 +5,9 @@ GROUP BY user_id;
 
 SELECT 
     property_id,
-    total_bookings,
-    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
-FROM (
-    SELECT 
-        property_id,
-        COUNT(*) AS total_bookings
-    FROM 
-        bookings
-    GROUP BY 
-        property_id
-) AS booking_counts;
+    COUNT(*) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM 
+    bookings
+GROUP BY 
+    property_id;
