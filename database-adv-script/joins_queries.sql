cda3f2353af0ccac911a03687555173fa6a70480
cikristian <rwibutsohakizinkachristian@gmail.com>
2025-05-19 04:13:33 +0200

    database JOIN queries added

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,14 @@
+SELECT *
+FROM bookings
+INNER JOIN users
+ON bookings.user_id = users.id
+
+SELECT *
+FROM properties
+LEFT JOIN properties
+ON properties.property_id = reviews.property_id
+
+SELECT *
+FROM users
+FULL OUTER JOIN bookings
+ON users.id = bookings.user_id
